68b27af832a33a69e5f703d4f6d3778cd5d16372
Simona <sima.simute1999@gmail.com>
2024-04-28 17:13:31 +0300

    Enemy AI

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 size = new Vector3(100, 1, 100);
+    
+    private NavMeshAgent navMeshAgent;
+    private bool isFollowing = false;
+    
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        InvokeRepeating(nameof(Wander), 0, Random.Range(1, 3));
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+        
+        if(Vector3.Distance(transform.position, target.position) < 30)
+        {
+            isFollowing = true;
+            navMeshAgent.SetDestination(target.position);
+        }
+        else
+        {
+            isFollowing = false;
+        }
+    }
+
+    void Wander()
+    {
+        if (isFollowing) return;
+        var randomPosition = new Vector3(Random.Range(-size.x, size.x),100, Random.Range(-size.z, size.z));
+        //find earth position
+        if(Physics.Raycast(randomPosition, Vector3.down, out var hit, 200))
+        {
+            randomPosition = hit.point;
+        }
+        navMeshAgent.SetDestination(randomPosition);
+    }
+}
